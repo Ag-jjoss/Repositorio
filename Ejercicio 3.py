e7052d689d54d5647544be9ba6bb36479871c8ae
Ag-jjoss <125081169+Ag-jjoss@users.noreply.github.com>
2023-08-11 19:02:45 -0600

    Update Ejercicio 3.py

--- Ejercicio 3.py
+++ Ejercicio 3.py
@@ -1,1 +1,26 @@
+"""
+Calculadora
+"""
+a = int(input("ingresa un numero"))
+b = int(input("ingresa otro numero"))
 
+print("operaciones disponibles")
+print("1.- Suma")
+print("2.- Resta")
+print("3.- Multpiplicacion ")
+print("4.- Division")
+operacion = int(input("Indica la operacion que quieres realizar :"))
+
+if  operacion == 1:
+    resultado = a + b
+elif operacion == 2:
+    resultado = a - b
+elif operacion == 3:
+    resultado = a * b
+elif operacion == 4:
+    if  b == 0:
+        resultado = print("no es posible divir entre 0")
+    else:
+        resultado = a / b
+
+print("El resultado es",resultado)
