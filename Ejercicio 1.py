14ee5df3275c85ff629504b34cee9c82876c3001
Ag-jjoss <125081169+Ag-jjoss@users.noreply.github.com>
2023-08-11 19:05:29 -0600

    Ejercicio 1.py

--- Ejercicio 1.py
+++ Ejercicio 1.py
@@ -1,14 +1,19 @@
 """
 Suma de pares
 """
-n = int(input("Escribe un número entero: "))
-m = int(input("Escribe un número entero mayor a n: "))
+def suma_numeros_pares_entre_n_m():
+    n = int(input("Escribe un número entero: "))
+    m = int(input("Escribe un número entero mayor a n: "))
+    
+    i = n
+    suma = 0
+    
+    while i <= m:
+        if i % 2 == 0:
+            suma += i
+        i += 1
+        
+    print("La suma de los números pares entre", n, "y", m, "es", suma)
 
-i = n
 
-suma = 0
-while i <= m:
-  if i % 2 == 0:
-    suma += i
-  i += 1
-print("La suma de los números pares entre ",n, "y", m, "es" ,suma)
+
